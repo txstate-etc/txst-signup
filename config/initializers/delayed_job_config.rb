0413c6c93ad3fc749bf505cdd1a2a9b24fd2afbb
Charles Jones <cj32@txstate.edu>
2016-03-11 10:37:56 -0600

    Used wrong `silence` method when squelching delayed_jobs logging.

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -4,11 +4,12 @@ Delayed::Worker.max_attempts = 10
 Delayed::Worker.max_run_time = 5.minutes
 Delayed::Worker.delay_jobs = !Rails.env.test?
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'delayed_job.log'))
+Delayed::Worker.logger.level = 0
 
 # Silence DJ querying AR frequently.
 module SilentJob
   def reserve(*args)
-    silence { super(*args) }
+    ActiveRecord::Base.logger.silence { super(*args) }
   end
 end
 Delayed::Backend::ActiveRecord::Job.singleton_class.prepend(SilentJob)
