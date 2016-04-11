5014ba9df1f3e26a2b0c3ca4622142abc2e6cd82
Charles Jones <cj32@txstate.edu>
2016-04-11 17:57:43 -0500

    Pulled txst specific config out of ldap logic.  #5876

--- config/initializers/defaults.rb
+++ config/initializers/defaults.rb
@@ -5,4 +5,6 @@ BASE_DN = "DC=matrix,DC=txstate,DC=edu"
 BIND_DN = "CN=sa-ldap-its,OU=Service Accounts,OU=TxState Objects,#{BASE_DN}"
 BIND_PASS = Rails.application.secrets.ldap_password
 USER_BASE = "ou=TxState Users,#{BASE_DN}"
+QUERY_BASE = "(objectCategory=CN=Person,CN=Schema,CN=Configuration,#{BASE_DN})"
 LDAP_SERVERS = ['ldap.txstate.edu']
+MAIL_DOMAIN = 'txstate.edu'
