4550968c1e6420fada4bbdde5e3dc4499dbc80eb
Yuanhua Qu <yuanhuaqu@yahoo.com>
2020-07-20 16:39:55 -0500

    Transfer all secrets to env

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,7 +14,6 @@ export DB_PORT=${DB_PORT:-3306}
 perl -i -pe 's/\Q{{RAILS_ENV}}\E/$ENV{RAILS_ENV}/' /etc/apache2/apache2.conf
 perl -i -pe 's/\Q{{WEB_HOSTNAME}}\E/$ENV{WEB_HOSTNAME}/' /etc/apache2/apache2.conf
 perl -i -pe 's/\Q{{SERVER_ADMIN}}\E/$ENV{SERVER_ADMIN}/' /etc/apache2/apache2.conf
-perl -i -pe 's/\Q{{SERVER_ADMIN}}\E/$ENV{SERVER_ADMIN}/' /usr/app/config/application.rb
 
 perl -i -pe 's/\Q{{sslkeyfile}}\E/glob("\/ssl\/*.key.pem")/e' /etc/apache2/apache2.conf
 perl -i -pe 's/\Q{{sslcertfile}}\E/glob("\/ssl\/*.cert.pem")/e' /etc/apache2/apache2.conf
