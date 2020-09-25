be2ba12376a6e61ce92c585db3467ee2445c9753
Yuanhua Qu <yuanhuaqu@yahoo.com>
2020-09-25 13:27:40 -0500

    Commenting out the log change as it throw error of ArgumentError: wrong number of arguments (0 for 1) in jobs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -67,7 +67,7 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = Logger.new(STDOUT)
+  #config.logger = Logger.new(STDOUT)
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
