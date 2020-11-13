30a9d1cddc4ced5bc59d6b86b93f1eda0ed65e15
Yuanhua Qu <yuanhuaqu@yahoo.com>
2020-11-13 12:24:30 -0600

    Fixing signup asset js and css compile and load correctly.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,4 +10,17 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
+//= require jquery_ujs
+//= require jquery-ui/ui/jquery.ui.core.js
+//= require jquery-ui/ui/jquery.ui.widget.js
+//= require jquery-ui/ui/jquery.ui.position.js
+//= require jquery-ui/ui/jquery.ui.button.js
+//= require jquery-ui/ui/jquery.ui.menu.js
+//= require jquery-ui/ui/jquery.ui.dialog.js
+//= require jquery-ui/ui/jquery.ui.autocomplete.js
+//= require jquery-ui/ui/jquery.ui.datepicker.js
+//= require jqueryui-timepicker-addon/dist/jquery-ui-timepicker-addon.js
+//= require autocomplete-rails
+//= require cocoon
 //= require_tree .
